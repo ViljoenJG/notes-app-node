531c2f9848d8a89181c5895af75438dee8c6b221
Kobus Viljoen <kobusv@spsi.co.za>
2017-06-06 14:23:24 +0200

    added user input

--- app.js
+++ app.js
@@ -4,4 +4,17 @@ const _ = require('lodash');
 const { errHandler, capitalise, add } = require('./handlers.js');
 const { addNote } = require('./notes');
 
-console.log(addNote());
+const command = process.argv[2];
+console.log(`Command: ${ command }`);
+
+if (command === 'add') {
+    console.log('adding new note');
+} else if (command === 'list') {
+    console.log('Listing notes');
+} else if (command === 'read') {
+    console.log('Reading Notes');
+} else if (command === 'remove') {
+    console.log('Removing note');
+} else {
+    console.error('Command not recognised');
+}
