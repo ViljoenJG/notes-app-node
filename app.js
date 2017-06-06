6345c5323106ada1b6dc17961fd8f40b7a0a1c8a
Kobus Viljoen <kobusv@spsi.co.za>
2017-06-06 14:52:12 +0200

    Simplifying arguments with Yargs

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
-const { errHandler, capitalise, add } = require('./handlers.js');
-const { addNote } = require('./notes');
+const { addNote, removeNote, getNote, getAllNotes } = require('./notes');
 
-const command = process.argv[2];
+const argv = yargs.argv;
+const command = argv._[0];
 console.log(`Command: ${ command }`);
+console.log('Yargs', argv);
 
 if (command === 'add') {
-    console.log('adding new note');
+    addNote(argv.title, argv.body);
 } else if (command === 'list') {
-    console.log('Listing notes');
+    getAllNotes();
 } else if (command === 'read') {
-    console.log('Reading Notes');
+    getNote(argv.title);
 } else if (command === 'remove') {
-    console.log('Removing note');
+    removeNote(argv.title);
 } else {
     console.error('Command not recognised');
 }
--- notes.js
+++ notes.js
@@ -1,4 +1,23 @@
-module.exports.addNote = function () {
-    console.log('addNote');
-    return 'New note';
+const addNote = (title, body) => {
+    console.log('Adding Note', title, body);
+    // return `Title: ${ title }. Body: ${ body }`
+};
+
+const getAllNotes = () => {
+    console.log('Getting all notes');
+};
+
+const getNote = title => {
+    console.log('Getting note', title);
+};
+
+const removeNote = title => {
+    console.log('Removing note', title);
+};
+
+module.exports = {
+    addNote,
+    getAllNotes,
+    getNote,
+    removeNote
 };
