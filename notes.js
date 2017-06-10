6b0ec3221233d64d5ff447fa9490b3934482a555
Kobus Viljoen <kobusv@gmail.com>
2017-06-10 11:17:35 +0200

    added comments to Notes methods.

--- notes.js
+++ notes.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
+// Takes note title and body as input.
+    // Checks if title exists before saving the note.
 module.exports.addNote = (title, body) => {
     let notes = fetchNotes();
 
@@ -22,22 +24,30 @@ module.exports.getAllNotes = () => {
     return fetchNotes();
 };
 
+// Returns note with matching title.
+    // Returns undefined if note is not found.
 module.exports.getNote = (title) => {
     let notes = fetchNotes();
     notes = notes.filter(note => note.title === title);
     return notes[0]
 };
 
+// Takes title as input and removes note with matching title.
+    // Returns Boolean to indicate if note was removed.
 module.exports.removeNote = (title) => {
     let notes = fetchNotes();
     let before = notes.length;
 
     notes = notes.filter(note => note.title !== title);
-    saveNotes(notes);
+
+    if (notes.length !== before) {
+        saveNotes(notes);
+    }
 
     return notes.length !== before;
 };
 
+// Logs note details to the console.
 module.exports.logNote = (message, note) => {
     console.log(message || 'Note');
     console.log('-----');
@@ -49,6 +59,9 @@ module.exports.logNote = (message, note) => {
 * Private methods
 * *********************/
 
+// Reads JSON data file, if it exists.
+    // If JSON is not an array, an empty array is returned.
+    // If file does not exist an empty array is returned.
 function fetchNotes() {
     try {
         let notes = JSON.parse(fs.readFileSync('data/notes-data.json'));
@@ -58,10 +71,13 @@ function fetchNotes() {
     }
 }
 
+// Takes notes array as input. Saves as JSON to file.
 function saveNotes(notes) {
     fs.writeFileSync('data/notes-data.json', JSON.stringify(notes));
 }
 
+// Returns notes array and title as inputs.
+    // Returns Boolean to indicate if a note by that title exists in the array.
 function noteExists(notes, title) {
     return notes.some(note => note.title === title);
 }
